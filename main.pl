95430bf168417840735810c06739aff14d502685
Hugh Rawlinson <hugh@rawlinson.com>
2011-06-05 15:49:52 +0400

    Second Commit. Includes main.pl

---
+++ main.pl
@@ -0,0 +1,24 @@
+#!/usr/local/bin/perl
+
+@lines;
+$break = 1;
+$n = 0;
+print "|| SYSTEM UP ||";
+
+sub the_loop{
+	#write this code
+	}
+
+while (1) {
+	$input = <STDIN>;
+	chomp($input);
+	if (scalar(@lines > 0){
+		the_loop();
+	}
+	else{
+		@lines[0] = @responses0;
+		@lines[0][0] = $input;
+		$prev = @lines[0][0];
+		print @lines[0][0];
+	}
+}
