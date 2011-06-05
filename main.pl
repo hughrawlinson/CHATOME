72a58db2ece92b80624b4483b5a8d90ddb01b3d5
Hugh Rawlinson <hugh@rawlinson.com>
2011-06-05 16:27:49 +0400

    third commit. updated main.pl

--- main.pl
+++ main.pl
@@ -1,9 +1,9 @@
-#!/usr/local/bin/perl
+#!/opt/local/bin/perl
 
 @lines;
 $break = 1;
 $n = 0;
-print "|| SYSTEM UP ||";
+print "|| SYSTEM UP ||\n";
 
 sub the_loop{
 	#write this code
@@ -11,14 +11,15 @@ sub the_loop{
 
 while (1) {
 	$input = <STDIN>;
+	print '\n';
 	chomp($input);
-	if (scalar(@lines > 0){
+	if (scalar(@lines > 0)){
 		the_loop();
 	}
 	else{
-		@lines[0] = @responses0;
+		push @{ $lines[0] = @responses0};
 		@lines[0][0] = $input;
 		$prev = @lines[0][0];
-		print @lines[0][0];
+		print @lines[0][0] . '\n';
 	}
 }
