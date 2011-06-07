447e8b5602fd1febb67f8126d3e66105dcffedc0
Hugh Rawlinson <hugh@rawlinson.com>
2011-06-07 15:02:38 +0400

    Reserved keyword 'reset'

--- main.pl
+++ main.pl
@@ -4,23 +4,31 @@
 $prev;
 $break = 1;
 $n = 0;
-print "|| SYSTEM UP || \n";
+print "|| SYSTEM UP ||\n";
+print "|| BE AWARE THAT 'reset' IS A RESERVED KEYWORD ||\n";
+
 
 sub the_loop{
-	#write this code
-	}
+	#if ($input != @lines[$n][0] || $n !> scalar(@lines)){
+	#
+	#}
+}
 
 while (1) {
 	$input = <STDIN>;
-	chomp($input);
-	if (scalar(@lines > 0)){
-		the_loop();
+	if ($input eq "reset"){
 	}
 	else{
-		@responses0;
-		push(@responses0, $input);
-		push(@lines, \@responses0);
-		$prev = $input;
-		print "$lines[0][0] \n";
+		chomp($input);
+		if (scalar(@lines > 0)){
+			the_loop();
+		}
+		else{
+			@responses0;
+			push(@responses0, $input);
+			push(@lines, \@responses0);
+			$prev = $input;
+			print "$lines[0][0] \n";
+		}
 	}
 }
